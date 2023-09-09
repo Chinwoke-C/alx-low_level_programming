be2e6076b043687d5832a3afc0d46fecaa6f7306
Chinwoke <canugwara99@gmail.com>
2023-09-09 09:53:27 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,18 +10,18 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
- if (n > 0) 
+if (n > 0)
 {
 printf("%d is positive\n", 98);
 }
- else if (n < 0) 
+else if (n < 0)
 {
 printf("%d is negative\n", -98);
 }
- else 
+else
 {
 printf("%d is zero\n", 0);
 }
 
- return 0;
+return (0);
 }
