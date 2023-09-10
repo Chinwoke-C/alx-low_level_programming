cf602a1de2f70c9f0f72c66eefe21af553d54d5a
Chinwoke <canugwara99@gmail.com>
2023-09-10 06:29:53 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,21 +6,12 @@
 int main(void)
 {
 char lowercase = 'a';
-char uppercase = 'A';
 while (lowercase <= 'z')
 {
-if (lowercase != 'e' && lowercase != 'q') 
-{
+if (lowercase != 'e' && lowercase != 'q')
 putchar(lowercase);
 }
 lowercase++;
-}
-while (uppercase <= 'Z')
-{
-if (uppercase != 'E' && uppercase != 'Q') {
-putchar(uppercase);
-}
-uppercase++;
 }
 return (0);
 }
