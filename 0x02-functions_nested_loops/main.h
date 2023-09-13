31f2dc41c05271d510836c6414a1de2ec6902b5b
Chinwoke <canugwara99@gmail.com>
2023-09-13 04:47:02 -0700

    4-isalpha

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,5 +6,6 @@ int _putchar(char c);
 void print_alphabet(void);
 int _islower(int c);
 void print_alphabet_x10(void);
+int _isalpha(char c);
 /* Add additional function prototypes here if needed */
 #endif /* MAIN_H */
