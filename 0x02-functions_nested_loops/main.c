71df8fca86e966624875b3a410be8c950f307eb0
Chinwoke <canugwara99@gmail.com>
2023-09-13 02:13:28 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void);
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
