8e1eaab0440f3a3aeeb461de1e43398b8cb2ea61
Chinwoke <canugwara99@gmail.com>
2023-09-13 15:18:52 -0700

    6-abs

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,5 +8,6 @@ int _islower(int c);
 void print_alphabet_x10(void);
 int _isalpha(char c);
 int print_sign(int n);
+int _abs(int i);
 /* Add additional function prototypes here if needed */
 #endif /* MAIN_H */
