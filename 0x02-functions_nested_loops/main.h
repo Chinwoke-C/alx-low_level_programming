51a73c8cd3f20c03ba53de8fccd60c7f7256023a
Chinwoke <canugwara99@gmail.com>
2023-09-13 14:21:29 -0700

    5-sign

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,6 @@ void print_alphabet(void);
 int _islower(int c);
 void print_alphabet_x10(void);
 int _isalpha(char c);
+int print_sign(int n);
 /* Add additional function prototypes here if needed */
 #endif /* MAIN_H */
