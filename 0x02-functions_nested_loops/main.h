87e4f79ba884e419a07dbe6c23b8a916649c9b53
Chinwoke <canugwara99@gmail.com>
2023-09-13 16:48:31 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,5 +13,6 @@ int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
+void print_to_98(int n);
 /* Add additional function prototypes here if needed */
 #endif /* MAIN_H */
